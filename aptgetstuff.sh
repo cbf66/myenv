82a59c21fcbba2b1129ca1e462d4eec2f08ff4fc
Charles Frankston <cbf@arxiv.org>
2025-03-15 20:02:13 -0400

    Add a bunch more things to aptgetstuff.sh

--- aptgetstuff.sh
+++ aptgetstuff.sh
@@ -1,4 +1,20 @@
+sudo apt update
+sudo apt upgrade -y
+sudo apt install -y git
+sudo apt install -y curl
+sudo apt install -y wget
+sudo apt install -y unzip
+sudo apt install -y zip
+sudo apt install -y tree
+sudo apt install -y htop
+sudo apt install -y nmap
+sudo apt install -y dnsutils
+sudo apt install -y net-tools
+sudo apt install -y iputils-ping
+sudo apt install -y iputils-tracepath
+sudo apt install -y iputils-traceroute
+sudo apt install -y iputils-arping
+sudo apt install -y iputils-clockdiff
 sudo apt install -y whois
-sudo apt install -y wslu   # wslview
-sudo apt install -y inetutils-traceroute
+#sudo apt install -y wslu   # wslview
 sudo apt install -y emacs
